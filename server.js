c35eaf1ca16609a1b51efc5ad3673220dbe9c48b
cody <codyjeromegreen@gmail.com>
2018-07-21 11:17:30 -0700

    got mlabs and mongodb hooked up about to make a user model

--- server.js
+++ server.js
@@ -2,19 +2,25 @@
 
 const express = require('express');
 const app = express();
-// require('dotenv').config();
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('mongo connected'))
+  .catch((err) => console.log(err));
 
 app.use('/', require('./routes/routes.js'));
 
 const PORT = process.env.PORT || 3000;
 
+
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    });
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
 }
 
 app.listen(PORT, () => {
-    console.log('http://localhost:' + PORT);
+  console.log('http://localhost:' + PORT);
 });
