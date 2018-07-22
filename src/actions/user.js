e886ba7765d551bededd7f5282d5c408d13ad110
cody <codyjeromegreen@gmail.com>
2018-07-22 08:28:17 -0700

    isbusiness user and decoded seems to be working

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,12 +1,20 @@
 export const USER_SIGNIN = 'USER_SIGNIN';
 export const USER_SIGNUP = 'USER_SIGNUP';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 
 export function userSignIn(json) {
   console.log('in actions json', json);
-  return {type: USER_SIGNIN, json}
+  return { type: USER_SIGNIN, json }
 }
 
 export function userSignUp(json) {
   console.log('in actions json', json);
-  return {type: USER_SIGNUP, json}
+  return { type: USER_SIGNUP, json }
+}
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
 }
