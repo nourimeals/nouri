bcc973d2bf8361414f297bd4dceb5c08cb420699
Rafael Malave <rafael.enrique.malave@gmail.com>
2018-07-22 08:58:44 -0700

    uncomment if statement

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ app.use('/api/v0/meals', require('./routes/meals.js'));
 const PORT = process.env.PORT || 3000;
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log('http://localhost:' + PORT);
